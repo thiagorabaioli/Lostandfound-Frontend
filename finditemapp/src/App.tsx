5c97eaa9f8d34656b77e81cd55fa54feb36f7f27
thiagorabaioli <tfrbaioli@outlook.com>
2025-05-27 21:35:11 +0100

    First commit

---
+++ finditemapp/src/App.tsx
@@ -0,0 +1,12 @@
+
+import LostDetailsCard from './components/LostDetailsCard';
+import Catalog from './routes/Catalog';
+import Lostandfound from './routes/Lostandfound';
+import ProductDetails from './routes/ProductDetails';
+
+export default function App() {
+
+  return (
+    < ProductDetails/>
+  );
+}
